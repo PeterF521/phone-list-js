00595e2ac0bf18c25b694e1c26c00f5d12d1f3da
PeterF521 <pedrofelgueiras2006@gmail.com>
2020-10-22 06:48:21 +0100

    config: Add settings and middlewares

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const config = require('config')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+module.exports = () => {
+    const app = express()
+
+    app.set('port', process.env.PORT || config.get('server.port'))
+    app.set('view engine', 'pug')
+    
+    app.use(bodyParser.json())
+
+    return app
+}
