ee90adbbfd8fe77db60518e29c342b9ae4733938
PeterF521 <pedrofelgueiras2006@gmail.com>
2020-11-14 07:26:33 +0000

    pages: Create submit status page

--- src/controllers/pageControllers.js
+++ src/controllers/pageControllers.js
@@ -13,3 +13,40 @@ exports.newUserPage = (req, res) => {
         contacts: users.users
     })
 }
+
+exports.newUser = (req, res) => {
+    try {
+        const contacts = handleFormData(req.body)
+        if(contacts === null) throw new Error('Invalid contact!')
+        const user = {
+            id: random(),
+            name: req.body.name,
+            contacts: contacts
+        }
+    
+        users.users.push(user)
+        data.save(users)
+
+        res.status(201).render('submitStatus', {
+            error: false,
+            userId: user.id
+        })
+    } catch(err) {
+        res.status(err.statusCode || 500).render('submitStatus', {
+            error: true,
+            msg: 'Oops! An error ocurred!'
+        })
+    }
+
+}
+function handleFormData(body) {
+    const rule = /(\d{9})|([a-z]+[0-9]*)[@]([a-z]+)[.]([a-z]+)/gm
+    const contactsArray = body.contacts.match(rule)
+
+    return contactsArray
+}
+
+function random() {
+    const length = users.users.length
+    return length + 1
+}
--- src/routes/pages.js
+++ src/routes/pages.js
@@ -3,6 +3,7 @@ const pageControllers = require('../controllers/pageControllers')
 module.exports = app => {
     app.get('/', pageControllers.get)
     app.get('/newUser', pageControllers.newUserPage)
+    app.post('/newUser/submit', pageControllers.newUser)
 
     return app
 }
